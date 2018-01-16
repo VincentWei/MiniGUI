db06a81773fa2f2b980784783be0696b7a05af2d
VincentWei <vincent@minigui.org>
2018-01-16 18:02:35 +0800

    not encrypted splash file

--- src/sysres/license/c_files/_splash_inner_res.c
+++ src/sysres/license/c_files/_splash_inner_res.c
@@ -12,11 +12,11 @@ extern unsigned char __mg_splash_dat_04_progressbar_bk_data[]; //04_progressbar-
 
 //declear arrays
 static INNER_RES __mg_splash_splash_inner_res[]={
-	{ 0x1562B2, (void*)__mg_splash_dat_00_minigui_data, 18067, "dat"}, //00_minigui.dat.c
-	{ 0xCD582, (void*)__mg_splash_dat_01_fmsoft_data, 3806, "dat"}, //01_fmsoft.dat.c
-	{ 0xC8712, (void*)__mg_splash_dat_02_feiman_data, 10032, "dat"}, //02_feiman.dat.c
-	{ 0x536B0E, (void*)__mg_splash_dat_03_progressbar_data, 2863, "dat"}, //03_progressbar.dat.c
-	{ 0xC97F8E, (void*)__mg_splash_dat_04_progressbar_bk_data, 2932, "dat"}, //04_progressbar-bk.dat.c
+	{ 0x1562B2, (void*)__mg_splash_dat_00_minigui_data, 18031, "dat"}, //00_minigui.dat.c
+	{ 0xCD582, (void*)__mg_splash_dat_01_fmsoft_data, 3798, "dat"}, //01_fmsoft.dat.c
+	{ 0xC8712, (void*)__mg_splash_dat_02_feiman_data, 10012, "dat"}, //02_feiman.dat.c
+	{ 0x536B0E, (void*)__mg_splash_dat_03_progressbar_data, 2857, "dat"}, //03_progressbar.dat.c
+	{ 0xC97F8E, (void*)__mg_splash_dat_04_progressbar_bk_data, 2926, "dat"}, //04_progressbar-bk.dat.c
 };
 
 #endif // _MG_ENABLE_LICENSE
