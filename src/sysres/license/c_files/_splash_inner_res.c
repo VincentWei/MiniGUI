a51748563b912b603032b5b31e24415852ed6db5
VincentWei <vincent@minigui.org>
2018-01-16 17:25:22 +0800

    default incore files for splash

---
+++ src/sysres/license/c_files/_splash_inner_res.c
@@ -0,0 +1,23 @@
+//inner list file
+//create by inner-trans
+
+#ifdef _MG_ENABLE_LICENSE
+
+//include files
+extern unsigned char __mg_splash_dat_00_minigui_data[]; //00_minigui.dat.c 
+extern unsigned char __mg_splash_dat_01_fmsoft_data[]; //01_fmsoft.dat.c 
+extern unsigned char __mg_splash_dat_02_feiman_data[]; //02_feiman.dat.c 
+extern unsigned char __mg_splash_dat_03_progressbar_data[]; //03_progressbar.dat.c 
+extern unsigned char __mg_splash_dat_04_progressbar_bk_data[]; //04_progressbar-bk.dat.c 
+
+//declear arrays
+static INNER_RES __mg_splash_splash_inner_res[]={
+	{ 0x1562B2, (void*)__mg_splash_dat_00_minigui_data, 18067, "dat"}, //00_minigui.dat.c
+	{ 0xCD582, (void*)__mg_splash_dat_01_fmsoft_data, 3806, "dat"}, //01_fmsoft.dat.c
+	{ 0xC8712, (void*)__mg_splash_dat_02_feiman_data, 10032, "dat"}, //02_feiman.dat.c
+	{ 0x536B0E, (void*)__mg_splash_dat_03_progressbar_data, 2863, "dat"}, //03_progressbar.dat.c
+	{ 0xC97F8E, (void*)__mg_splash_dat_04_progressbar_bk_data, 2932, "dat"}, //04_progressbar-bk.dat.c
+};
+
+#endif // _MG_ENABLE_LICENSE
+
