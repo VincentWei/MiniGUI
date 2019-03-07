dcd31bed7cdb68ab7ff7e72e9328c4cd7f0efd03
Vincent Wei <vincent@minigui.org>
2019-03-07 11:02:51 +0800

    Indic shaping table

--- src/font/utils/fetch-ucd.sh
+++ src/font/utils/fetch-ucd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 url='https://www.unicode.org/Public/11.0.0/ucd/'
-files='UnicodeData.txt IndicPositionalCategory.txt IndicSyllabicCategory.txt ArabicShaping.txt BidiBrackets.txt BidiMirroring.txt Blocks.txt'
+files='Scripts.txt UnicodeData.txt IndicPositionalCategory.txt IndicSyllabicCategory.txt ArabicShaping.txt BidiBrackets.txt BidiMirroring.txt Blocks.txt'
 
 if test ! -d ucd; then
     mkdir ucd
