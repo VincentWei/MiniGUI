fdb2ef360a4b48b32ae6f60775752fa1da1a4672
Vincent Wei <vincent@minigui.org>
2020-01-06 16:54:56 +0800

    cleanup: comment out unused fields in G_RES

--- src/include/sharedres.h
+++ src/include/sharedres.h
@@ -11,35 +11,35 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 /*
- *   This file is part of MiniGUI, a mature cross-platform windowing 
+ *   This file is part of MiniGUI, a mature cross-platform windowing
  *   and Graphics User Interface (GUI) support system for embedded systems
  *   and smart IoT devices.
- * 
- *   Copyright (C) 2002~2018, Beijing FMSoft Technologies Co., Ltd.
+ *
+ *   Copyright (C) 2002~2020, Beijing FMSoft Technologies Co., Ltd.
  *   Copyright (C) 1998~2002, WEI Yongming
- * 
+ *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
- * 
+ *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
- * 
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *   Or,
- * 
+ *
  *   As this program is a library, any link to this program must follow
  *   GNU General Public License version 3 (GPLv3). If you cannot accept
  *   GPLv3, you need to be licensed from FMSoft.
- * 
+ *
  *   If you have got a commercial license of this program, please use it
  *   under the terms and conditions of the commercial license.
- * 
+ *
  *   For more information about the commercial license, please refer to
  *   <http://www.minigui.com/blog/minigui-licensing-policy/>.
  */
@@ -95,38 +95,31 @@ typedef struct tagG_RES {
     int mousebutton;
     int shiftstatus;
     Uint8 kbd_state [MGUI_NR_KEYS + 1];
-    
+
 #ifdef _MGHAVE_CURSOR
     int cursorx, cursory;
     int oldboxleft, oldboxtop;
     HCURSOR csr_current;
     int xhotspot, yhotspot;
-	int csr_show_count;
+    int csr_show_count;
+
+    int csrnum;
+    unsigned long svdbitsoffset;
+    unsigned long csroffset;
 #endif
 
-#ifdef _MGHAVE_CURSOR
-	int csrnum;
-#endif
 /*
-	int iconnum;
-	int bmpnum;
-*/
-	int sysfontnum;
-	int rbffontnum;
-	int varfontnum;
+    int sysfontnum;
+    int rbffontnum;
+    int varfontnum;
+    int iconnum;
+    int bmpnum;
 
-#ifdef _MGHAVE_CURSOR
-	unsigned long svdbitsoffset;
-	unsigned long csroffset;
-#endif
-/*
-	unsigned long iconoffset;
-*/
-	unsigned long sfontoffset;
-	unsigned long rfontoffset;
-	unsigned long vfontoffset;
-/*
-	unsigned long bmpoffset;
+    unsigned long sfontoffset;
+    unsigned long rfontoffset;
+    unsigned long vfontoffset;
+    unsigned long iconoffset;
+    unsigned long bmpoffset;
 */
 
 } G_RES;
