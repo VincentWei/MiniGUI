9add6a46da736ff1497b1a99b32b4784d12a53dc
Vincent Wei <vincent@minigui.org>
2020-01-10 11:05:45 +0800

    enhance __mg_load_cursor_from_res to support PNG for compositing schema

--- src/sysres/incoreres.c
+++ src/sysres/incoreres.c
@@ -11,40 +11,40 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 /*
- *   This file is part of MiniGUI, a mature cross-platform windowing 
+ *   This file is part of MiniGUI, a mature cross-platform windowing
  *   and Graphics User Interface (GUI) support system for embedded systems
  *   and smart IoT devices.
- * 
+ *
  *   Copyright (C) 2002~2018, Beijing FMSoft Technologies Co., Ltd.
  *   Copyright (C) 1998~2002, WEI Yongming
- * 
+ *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
- * 
+ *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
- * 
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *   Or,
- * 
+ *
  *   As this program is a library, any link to this program must follow
  *   GNU General Public License version 3 (GPLv3). If you cannot accept
  *   GPLv3, you need to be licensed from FMSoft.
- * 
+ *
  *   If you have got a commercial license of this program, please use it
  *   under the terms and conditions of the commercial license.
- * 
+ *
  *   For more information about the commercial license, please refer to
  *   <http://www.minigui.com/blog/minigui-licensing-policy/>.
  */
 /*
-** resource.c: This file include some functions for system resource loading. 
+** resource.c: This file include some functions for system resource loading.
 **           some functions are from misc.c.
 **
 ** Create date: 2003/09/06
@@ -68,8 +68,9 @@
 #include "cursor.h"
 #include "icon.h"
 #include "sysres.h"
+#include "misc.h"
 
-#ifdef _MGINCORE_RES 
+#ifdef _MGINCORE_RES
 
 #include "../font/rawbitmap.h"
 #include "../font/varbitmap.h"
@@ -97,11 +98,18 @@ BOOL sysres_init_inner_resource()
 extern const int __mg_cursors_offset [];
 extern const unsigned char __mg_cursors_data[];
 #endif
-//HCURSOR GUIAPI LoadCursorFromRes (int i)
-HCURSOR mg_LoadCursorFromRes (int i)
+
+HCURSOR __mg_load_cursor_from_res (int i)
 {
     HCURSOR hCursor = 0;
     int nr_cursors = 0;
+    char key [32];
+    char filename [MAX_NAME + 1];
+#ifdef _MGUSE_COMPOSITING
+    const char* ext;
+    BOOL is_png = FALSE;
+    int hotspot[2] = { 0, 0 };
+#endif
 
     if (GetMgEtcIntValue ("cursorinfo", "cursornumber", &nr_cursors) < 0 )
         return 0;
@@ -109,29 +117,48 @@ HCURSOR mg_LoadCursorFromRes (int i)
     if (i >= nr_cursors || i < 0)
         return 0;
 
+    sprintf (key, "cursor%d", i);
+    if (GetMgEtcValue (CURSORSECTION, key, filename, MAX_NAME) < 0)
+        return 0;
+
+#ifdef _MGUSE_COMPOSITING
+    ext = __mg_get_extension (filename);
+    if (ext && strcasecmp (ext, "png") == 0) {
+        sprintf (key, "hotspot%d", i);
+        if (GetMgEtcValue (CURSORSECTION, key, filename, MAX_NAME) < 0)
+            return 0;
+
+        __mg_extract_integers (filename, ',', hotspot, 2);
+        is_png = TRUE;
+    }
+#endif
+
 #ifdef _MGINCORE_RES
+#   ifdef _MGUSE_COMPOSITING
+    if (is_png)
+        hCursor = LoadCursorFromPNGMem (__mg_cursors_data + __mg_cursors_offset [i],
+                hotspot[0], hotspot[1]);
+    else
+#   endif
     hCursor = LoadCursorFromMem (__mg_cursors_data + __mg_cursors_offset [i]);
 #endif
 
     if (!hCursor) {
-        char filename[MAX_NAME + 1];
-        char path[MAX_PATH + 1];
-        char key[10];
+        char path [MAX_PATH + 1];
 
         strncpy (path, sysres_get_system_res_path(), MAX_PATH);
-        strcat(path, "cursor/");
-
-        sprintf (key, "cursor%d", i);
-        if (GetMgEtcValue (CURSORSECTION, key, filename, MAX_NAME) < 0)
-            return 0;
-
+        strcat (path, "cursor/");
         strcat (path, filename);
-        hCursor = LoadCursorFromFile (path);
 
+#ifdef _MGUSE_COMPOSITING
+        if (is_png)
+            hCursor = LoadCursorFromPNGFile (path, hotspot[0], hotspot[1]);
+        else
+#endif
+        hCursor = LoadCursorFromFile (path);
     }
 
     return hCursor;
 }
 #endif
-
 
