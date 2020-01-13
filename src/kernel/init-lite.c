e98032331fc60628b4efef71a2de216f64024057
Vincent Wei <vincent@minigui.org>
2020-01-13 10:21:23 +0800

    initial screen resolution in shared resource segment

--- src/kernel/init-lite.c
+++ src/kernel/init-lite.c
@@ -486,6 +486,8 @@ int InitGUI (int argc, const char* agr[])
             *SHAREDRES_VIDEO_EXDRIVER = 0;
 
         SHAREDRES_VIDEO_DPI   = __gal_screen->dpi;
+        SHAREDRES_VIDEO_HRES  = __gal_screen->w;
+        SHAREDRES_VIDEO_VRES  = __gal_screen->h;
         SHAREDRES_VIDEO_DEPTH = __gal_screen->format->BytesPerPixel;
         SHAREDRES_VIDEO_RMASK = __gal_screen->format->Rmask;
         SHAREDRES_VIDEO_GMASK = __gal_screen->format->Gmask;
