dc58ee6d20eb60f1591d093b37563d57e672eaf5
Vincent Wei <vincent@minigui.org>
2020-01-15 12:29:45 +0800

    some new flags for znode

--- src/include/zorder.h
+++ src/include/zorder.h
@@ -67,29 +67,40 @@ typedef struct _MASKRECT {
 } MASKRECT;
 typedef MASKRECT* PMASKRECT;
 
-#define ZOF_REFERENCE       0x00000001
-#define ZOF_VISIBLE         0x00000002
-#define ZOF_DISABLED        0x00000004
-#define ZOF_FLAG_MASK       0x0000000F
+#define ZOF_FLAG_MASK           0x0000000F
+#define ZOF_REFERENCE           0x00000001
+#define ZOF_VISIBLE             0x00000002
+#define ZOF_DISABLED            0x00000004
 
-#define ZOF_TYPE_DESKTOP    0x50000000
-#define ZOF_TYPE_MENU       0x40000000
-#define ZOF_TYPE_GLOBAL     0x30000000
-#define ZOF_TYPE_TOPMOST    0x20000000
-#define ZOF_TYPE_NORMAL     0x10000000
-#define ZOF_TYPE_MASK       0xF0000000
+/* Since 4.2.0 */
+#define ZOF_LUCENCY_MASK        0x000000F0
+#define ZOF_LT_OPAQUE           0x00000000
+#define ZOF_LT_ALPHACHANNEL     0x00000010
+#define ZOF_LT_BLURRED          0x00000030
+#define ZOF_LT_ALPHAPIXEL       0x00000020
 
-#define ZOF_TF_MAINWIN      0x01000000
-#define ZOF_TF_TOOLWIN      0x02000000
-/* TOPFOREVER can be composite with other type.*/
-#define ZOF_TF_TOPFOREVER   0x04000000
+/* Since 4.2.0; The alpha channel value or the radius of blur in pixels */
+#define ZOF_ALPHA_BLUR_MASK     0x0000FF00
 
-#define ZOF_TW_TBROUNDCNS   0x00300000
-#define ZOF_TW_BROUNDCNS    0x00200000
-#define ZOF_TW_TROUNDCNS    0x00100000
+#define ZOF_TYPE_FLAG_MASK      0xFFF00000
 
-#define ZOF_TW_FLAG_MASK    0x00F00000
-#define ZOF_TYPE_FLAG_MASK  0xFFF00000
+#define ZOF_TYPE_MASK           0xF0000000
+#define ZOF_TYPE_DOCKER         0x60000000  /* Since 4.2.0 */
+#define ZOF_TYPE_DESKTOP        0x50000000
+#define ZOF_TYPE_MENU           0x40000000
+#define ZOF_TYPE_GLOBAL         0x30000000
+#define ZOF_TYPE_TOPMOST        0x20000000
+#define ZOF_TYPE_NORMAL         0x10000000
+
+#define ZOF_TF_FLAG_MASK        0x0F000000
+#define ZOF_TF_MAINWIN          0x01000000
+#define ZOF_TF_TOOLWIN          0x02000000
+#define ZOF_TF_TOPFOREVER       0x04000000
+
+#define ZOF_TW_FLAG_MASK        0x00F00000
+#define ZOF_TW_TBROUNDCNS       0x00300000
+#define ZOF_TW_BROUNDCNS        0x00200000
+#define ZOF_TW_TROUNDCNS        0x00100000
 
 typedef struct _ZORDERNODE
 {
