e0537490c0786f2fa8aeeab4dc030178ed2090ca
Vincent Wei <vincent@minigui.org>
2020-01-19 16:23:10 +0800

    empty implementation of fallback compositor

---
+++ src/kernel/compsor-fallback.c
@@ -0,0 +1,90 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
+/*
+ *   This file is part of MiniGUI, a mature cross-platform windowing
+ *   and Graphics User Interface (GUI) support system for embedded systems
+ *   and smart IoT devices.
+ *
+ *   Copyright (C) 2002~2020, Beijing FMSoft Technologies Co., Ltd.
+ *   Copyright (C) 1998~2002, WEI Yongming
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Or,
+ *
+ *   As this program is a library, any link to this program must follow
+ *   GNU General Public License version 3 (GPLv3). If you cannot accept
+ *   GPLv3, you need to be licensed from FMSoft.
+ *
+ *   If you have got a commercial license of this program, please use it
+ *   under the terms and conditions of the commercial license.
+ *
+ *   For more information about the commercial license, please refer to
+ *   <http://www.minigui.com/blog/minigui-licensing-policy/>.
+ */
+/*
+** compsor-manager.c: the compositor manager module for MiniGUI.
+**
+** Create date: 2020-01-19
+**
+** Current maintainer: Wei Yongming.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define _DEBUG
+#include "common.h"
+
+#if defined(_MGRM_PROCESSES) && defined(_MGSCHEMA_COMPOSITING)
+
+#include "minigui.h"
+#include "gdi.h"
+
+static CompositorCtxt* initialize (const char* name)
+{
+    _DBG_PRINTF("called\n");
+    return (CompositorCtxt*)1;
+}
+
+static void terminate (CompositorCtxt* ctxt)
+{
+    _DBG_PRINTF("called\n");
+}
+
+void refresh (CompositorCtxt* ctxt)
+{
+    _DBG_PRINTF("called\n");
+}
+
+CompositorOps __mg_fallback_compositor = {
+    initialize: initialize,
+    terminate: terminate,
+    refresh: refresh,
+};
+
+#endif /* defined(_MGRM_PROCESSES) && defined(_MGSCHEMA_COMPOSITING) */
+
