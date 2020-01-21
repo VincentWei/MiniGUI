1b5e3e3a45014b5b2aede9e514f9555635d2ad7f
Vincent Wei <vincent@minigui.org>
2020-01-21 10:30:36 +0800

    cleanup: uppercase macros

--- src/include/drawsemop.h
+++ src/include/drawsemop.h
@@ -11,35 +11,35 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 /*
- *   This file is part of MiniGUI, a mature cross-platform windowing 
+ *   This file is part of MiniGUI, a mature cross-platform windowing
  *   and Graphics User Interface (GUI) support system for embedded systems
  *   and smart IoT devices.
- * 
- *   Copyright (C) 2002~2018, Beijing FMSoft Technologies Co., Ltd.
+ *
+ *   Copyright (C) 2002~2020, Beijing FMSoft Technologies Co., Ltd.
  *   Copyright (C) 1998~2002, WEI Yongming
- * 
+ *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
- * 
+ *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
- * 
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  *   Or,
- * 
+ *
  *   As this program is a library, any link to this program must follow
  *   GNU General Public License version 3 (GPLv3). If you cannot accept
  *   GPLv3, you need to be licensed from FMSoft.
- * 
+ *
  *   If you have got a commercial license of this program, please use it
  *   under the terms and conditions of the commercial license.
- * 
+ *
  *   For more information about the commercial license, please refer to
  *   <http://www.minigui.com/blog/minigui-licensing-policy/>.
  */
@@ -73,16 +73,16 @@ again:
     }
 }
 
-#define lock_draw_sem()     my_sem_op(_IDX_SEM_DRAW, -1)
-#define unlock_draw_sem()   my_sem_op(_IDX_SEM_DRAW, 1)
+#define LOCK_DRAW_SEM()     my_sem_op(_IDX_SEM_DRAW, -1)
+#define UNLOCK_DRAW_SEM()   my_sem_op(_IDX_SEM_DRAW, 1)
 
-#define lock_scr_sem()      my_sem_op(_IDX_SEM_SCR, -1)
-#define unlock_scr_sem()    my_sem_op(_IDX_SEM_SCR, 1)
+#define LOCK_SCREEN_SEM()   my_sem_op(_IDX_SEM_SCR, -1)
+#define UNLOCK_SCREEN_SEM() my_sem_op(_IDX_SEM_SCR, 1)
 
 #ifdef _MGHAVE_CURSOR
 
-#define lock_cursor_sem()   my_sem_op(_IDX_SEM_CURSOR, -1)
-#define unlock_cursor_sem() my_sem_op(_IDX_SEM_CURSOR, 1)
+#define LOCK_CURSOR_SEM()   my_sem_op(_IDX_SEM_CURSOR, -1)
+#define UNLOCK_CURSOR_SEM() my_sem_op(_IDX_SEM_CURSOR, 1)
 
 static inline int get_sem_pid (int num)
 {
@@ -148,8 +148,8 @@ again:
 #endif /* _MGHAVE_CURSOR */
 
 #ifdef _MGRM_PROCESSES
-#   define lock_mousemove_sem()     my_sem_op(_IDX_SEM_MOUSEMOVE, -1)
-#   define unlock_mousemove_sem()   my_sem_op(_IDX_SEM_MOUSEMOVE, 1)
+#   define LOCK_MOUSEMOVE_SEM()     my_sem_op(_IDX_SEM_MOUSEMOVE, -1)
+#   define UNLOCK_MOUSEMOVE_SEM()   my_sem_op(_IDX_SEM_MOUSEMOVE, 1)
 #endif /* _MGRM_PROCESSES */
 
 #ifdef __cplusplus
