1aa5f30f1946464c9b6e53375cd5da37328ecee5
Vincent Wei <vincent@minigui.org>
2020-02-01 21:36:46 +0800

    change MAX_SYS_REQID to 0x0022

--- include/minigui.h
+++ include/minigui.h
@@ -1675,7 +1675,7 @@ const CompositorOps* __ex_compositor_get (const char* name,
  *
  * \sa RegisterRequestHandler
  */
-#define MAX_SYS_REQID           0x0020
+#define MAX_SYS_REQID           0x0022
 
 /**
  * \def MAX_REQID
