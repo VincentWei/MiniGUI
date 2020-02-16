9c2be6c69b48d88752603131d44b6580d7a2a2b0
Vincent Wei <vincent@minigui.org>
2020-02-16 18:45:51 +0800

    cleanup

--- src/include/mgsock.h
+++ src/include/mgsock.h
@@ -52,7 +52,7 @@
 #define mg_fd_clr(fd, set)     FD_CLR(fd, set)
 #define mg_fd_isset(fd, set)   FD_ISSET(fd, set)
 #define mg_fd_zero(set)        FD_ZERO(set)
-#define mg_select(nfds, readfds, writefds, exceptfds, timeout) \
+#define mg_select(nfds, readfds, writefds, exceptfds, timeout)  \
     select(nfds, readfds, writefds, exceptfds, timeout)
 #define mg_readsocket(s, buf, len)     read(s, buf, len)
 #define mg_writesocket(s, buf, len)    write(s, buf, len)
@@ -63,7 +63,7 @@
 #define mg_fd_clr(fd, set)     win_fd_clr(fd, set)
 #define mg_fd_isset(fd, set)   win_fd_isset(fd, set)
 #define mg_fd_zero(set)        win_fd_zero(set)
-#define mg_select(nfds, readfds, writefds, exceptfds, timeout) \
+#define mg_select(nfds, readfds, writefds, exceptfds, timeout)  \
     win_select(nfds, readfds, writefds, exceptfds, timeout)
 #define mg_readsocket(s, buf, len)     win_readsock(s, buf, len, 0)
 #define mg_writesocket(s, buf, len)    win_writesock(s, buf, len, 0)
@@ -87,7 +87,8 @@ int win_fd_set (int fd, void *set);
 int win_fd_clr (int fd, void *set);
 int win_fd_isset (int fd, void *set);
 int win_fd_zero (void *set);
-int win_select (int nfds, void *readfds, void *writefds, void *exceptfds, const struct timeval *timeout);
+int win_select (int nfds, void *readfds, void *writefds, void *exceptfds,
+        const struct timeval *timeout);
 int win_readsock (int s, char *buf, int len, int flags);
 int win_writesock (int s, char *buf, int len, int flags);
 void win_closesock (int s);
